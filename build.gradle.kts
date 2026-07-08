ce9a385394ea9077fb23b6411e4fa0cbd7d1a064
WhiteSprite <1456014916@qq.com>
2026-07-08 23:17:50 +0800

    chore(build)：更新构建依赖和工具版本
    - 升级Gradle Wrapper版本至9.6.1
    - 更新Kotlin JVM及相关插件至2.4.0
    - 升级Micronaut核心及组件（如micronaut-data、micronaut-security等）版本
    - 更新数据库驱动、监控和日志相关依赖库版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 plugins {
-    kotlin("jvm") version "2.3.21" apply false
-    id("com.google.devtools.ksp") version "2.3.8" apply false
-    kotlin("plugin.allopen") version "2.3.21" apply false
-    id("io.micronaut.application") version "5.0.0" apply false
-    id("org.graalvm.buildtools.native") version "0.11.5" apply false
+    kotlin("jvm") version "2.4.0" apply false
+    id("com.google.devtools.ksp") version "2.3.9" apply false
+    kotlin("plugin.allopen") version "2.4.0" apply false
+    id("io.micronaut.application") version "5.0.2" apply false
+    id("org.graalvm.buildtools.native") version "1.1.3" apply false
 }
 
 tasks.wrapper {
-    gradleVersion = "9.5.1"
+    gradleVersion = "9.6.1"
     distributionType = Wrapper.DistributionType.ALL
     distributionUrl = "https://mirrors.cloud.tencent.com/gradle/gradle-${gradleVersion}-all.zip"
 }
