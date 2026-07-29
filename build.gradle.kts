20405c95226f378b21da31ccbf0f4114bf26883d
刘军 <1456014916@qq.com>
2026-07-29 16:08:35 +0800

    chore(deps): 升级 Micronaut 和 GraalVM 构建工具版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("com.google.devtools.ksp") version "2.3.10" apply false
     kotlin("plugin.allopen") version "2.4.10" apply false
     id("io.micronaut.application") version "5.0.2" apply false
-    id("org.graalvm.buildtools.native") version "1.1.3" apply false
+    id("org.graalvm.buildtools.native") version "1.1.6" apply false
 }
 
 tasks.wrapper {
