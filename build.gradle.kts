141a3e4904565962f10e977cde2b17e9f25e2128
刘军 <1456014916@qq.com>
2026-08-06 17:14:59 +0800

    chore(deps): 升级插件和依赖版本
    - 升级 KSP 和 GraalVM 构建工具版本
    - 升级 Gradle 包装器版本到 9.7.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 plugins {
     kotlin("jvm") version "2.4.10" apply false
-    id("com.google.devtools.ksp") version "2.3.10" apply false
+    id("com.google.devtools.ksp") version "2.3.11" apply false
     kotlin("plugin.allopen") version "2.4.10" apply false
     id("io.micronaut.application") version "5.0.2" apply false
-    id("org.graalvm.buildtools.native") version "1.1.6" apply false
+    id("org.graalvm.buildtools.native") version "1.1.7" apply false
 }
 
 tasks.wrapper {
-    gradleVersion = "9.6.1"
+    gradleVersion = "9.7.0"
     distributionType = Wrapper.DistributionType.ALL
     distributionUrl = "https://mirrors.cloud.tencent.com/gradle/gradle-${gradleVersion}-all.zip"
 }
