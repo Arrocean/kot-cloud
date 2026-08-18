a3b57a091ad127b7f72f7e8e98a1ae9ac7ccd820
WhiteSprite <1456014916@qq.com>
2026-08-18 17:53:27 +0800

    chore(deps)：升级 Micronaut 与 GraalVM 相关依赖版本
    - 将 GraalVM Native Build Tools 插件从 1.1.7 升级至 1.1.9
    - 将 Micronaut Platform 从 5.0.4 升级至 5.1.0
    - 将 Micronaut OpenAPI、Test、Tracing OpenTelemetry 分别升级至 7.1.2、5.1.0、8.2.0
    - 将 JUnit 从 6.1.1 升级至 6.1.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("com.google.devtools.ksp") version "2.3.11" apply false
     kotlin("plugin.allopen") version "2.4.10" apply false
     id("io.micronaut.application") version "5.0.2" apply false
-    id("org.graalvm.buildtools.native") version "1.1.7" apply false
+    id("org.graalvm.buildtools.native") version "1.1.9" apply false
 }
 
 tasks.wrapper {
